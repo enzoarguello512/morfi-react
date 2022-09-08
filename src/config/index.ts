56009fea21a817054bbba9f84777fff3bb7c7008
Enzo Arguello <arguelar@gmail.com>
2022-09-08 12:43:25 -0300

    fix: separate types of ports.

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,9 @@
 const config = {
-  PORT: process.env.PORT || '8080',
+  PORT: process.env.PORT || '3000',
   // Is usually the same domain as the backend. (process.env.BACKEND_DOMAIN)
   SOCKETIO_DOMAIN: process.env.REACT_APP_SOCKETIO_DOMAIN || '0.0.0.0',
   BACKEND_DOMAIN: process.env.REACT_APP_BACKEND_DOMAIN || '0.0.0.0',
+  BACKEND_PORT: process.env.REACT_APP_BACKEND_PORT || '8080',
 };
 console.log(config);
 
--- src/services/Socket.tsx
+++ src/services/Socket.tsx
@@ -4,7 +4,7 @@ import config from 'config';
 let domain: string = config.SOCKETIO_DOMAIN;
 
 if (process.env.NODE_ENV === 'development') {
-  domain = `${config.SOCKETIO_DOMAIN}:${config.PORT}`;
+  domain = `${config.SOCKETIO_DOMAIN}:${config.BACKEND_PORT}`;
 }
 
 export const socket = socketClient(domain);
