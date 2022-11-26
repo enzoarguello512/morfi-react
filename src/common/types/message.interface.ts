95e9b6484f3c9a1c7a9c631c8c16c629340a692a
Enzo Arguello <arguelar@gmail.com>
2022-11-26 06:33:03 -0300

    feat: added message interface.

---
+++ src/common/types/message.interface.ts
@@ -0,0 +1,10 @@
+export interface IMessage {
+  id: string;
+  user: string;
+  text: string;
+  type: TMessageType;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export type TMessageType = 'user' | 'server';
