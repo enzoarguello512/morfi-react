c0c5a296b641e98c8c872767cf99c0191e223bb9
Enzo Arguello <arguelar@gmail.com>
2022-12-09 13:40:41 -0300

    fix!: switch to regular endpoints in product api.

--- src/features/products/productsApiSlice.tsx
+++ src/features/products/productsApiSlice.tsx
@@ -1,57 +1,20 @@
-import {
-  createEntityAdapter,
-  createSelector,
-  EntityState,
-} from '@reduxjs/toolkit';
 import { apiSlice } from 'app/api/apiSlice';
-import { TRootState } from 'app/store';
-import { IProduct } from 'common/types/product.interface';
-
-const productsAdapter = createEntityAdapter<IProduct>();
-
-const initialState = productsAdapter.getInitialState();
+import {
+  IProduct,
+  IProductFiltersResponse,
+} from 'common/types/product.interface';
 
 export const productApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
-    list: builder.query<EntityState<IProduct>, void>({
-      transformResponse: (responseData: Array<IProduct>) => {
-        return productsAdapter.setAll(initialState, responseData);
-      },
-      query: () => '/products',
-      providesTags: (result, error, arg) => [
-        ...result.ids.map((id: string) => ({ type: 'Product' as const, id })),
-        { type: 'Product', id: 'LIST' },
-      ],
+    // Product
+    ///////////////////////////
+    list: builder.query<IProductFiltersResponse, string>({
+      query: (query) => `/products/filter${query}`,
     }),
-    //deleteById: builder.mutation({
-    //transformResponse: (responseData, meta, arg) => {
-    //return productsAdapter.removeOne(initialState, arg);
-    //},
-    //query: (productId) => ({
-    //url: `/products/${productId}`,
-    //method: 'DELETE',
-    //}),
-    //invalidatesTags: (result, error, arg) => [{ type: 'Product', id: arg }],
-    //}),
+    readById: builder.query<IProduct, string>({
+      query: (id) => `/products/${id}`,
+    }),
   }),
 });
 
-export const { useListQuery } = productApiSlice;
-
-// returns the query result object
-export const selectProductsResult = productApiSlice.endpoints.list.select();
-
-// Creates memoized selector
-const selectProductsData = createSelector(
-  selectProductsResult,
-  (productsResult) => productsResult.data // normalized state object with ids & entities
-);
-
-//getSelectors creates these selectors and we rename them with aliases using destructuring
-export const {
-  selectAll: selectAllProducts,
-  selectById: selectProductById,
-  // Pass in a selector that returns the products slice of state
-} = productsAdapter.getSelectors(
-  (state: TRootState) => selectProductsData(state) ?? initialState
-);
+export const { useListQuery, useReadByIdQuery } = productApiSlice;
