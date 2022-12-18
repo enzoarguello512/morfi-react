4b3c643ca831c3313a5b7986569486667b4c8441
Enzo Arguello <arguelar@gmail.com>
2022-12-17 21:28:13 -0300

    fix: improve user interaction.

--- src/components/CartSubtotal/CartSubtotal.tsx
+++ src/components/CartSubtotal/CartSubtotal.tsx
@@ -8,12 +8,14 @@ import { useAppDispatch, useAppSelector } from 'hooks/preTyped';
 import { toast } from 'react-toastify';
 
 const CartSubtotal = ({ productsQuantity, subtotal }) => {
-  const [updateCart] = useUpdateCartMutation();
+  const [updateCart, { isLoading: isUpdating }] = useUpdateCartMutation();
   const [createOrder, { isLoading }] = useCreateOrderMutation();
   const dispatch = useAppDispatch();
   const user: IUser = useAppSelector(selectCurrentUser);
 
-  const handleOrder = async (e) => {
+  const handleOrder = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
     try {
       //const payload = await createOrder({
       await createOrder({
@@ -26,6 +28,7 @@ const CartSubtotal = ({ productsQuantity, subtotal }) => {
           products: [],
         })
       );
+      toast.success('Order completed successfully!');
 
       await updateCart({
         cartId: user.cart.id,
@@ -51,8 +54,21 @@ const CartSubtotal = ({ productsQuantity, subtotal }) => {
       </p>
       <p className="border-bottom pb-3 px-3">${subtotal.toFixed(2)}</p>
       <div className="px-3 pb-3 text-center">
-        <button className="btn btn-dark" onClick={handleOrder}>
-          Proceed To Checkout
+        <button
+          className="btn btn-dark w-100"
+          onClick={handleOrder}
+          disabled={isLoading || isUpdating ? true : false}
+        >
+          {isLoading || isUpdating ? (
+            <div
+              className="spinner-border spinner-border-sm text-center"
+              role="status"
+            >
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          ) : (
+            'Create Order'
+          )}
         </button>
       </div>
     </div>
