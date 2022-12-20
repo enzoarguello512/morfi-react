5a5afafb5c90e751432cb179c205b5f8c71f977e
Enzo Arguello <arguelar@gmail.com>
2022-12-20 19:32:00 -0300

    fix: add module "react-helmet-async".

--- src/index.tsx
+++ src/index.tsx
@@ -13,31 +13,36 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { HelmetProvider } from 'react-helmet-async';
+import { Helmet } from 'react-helmet-async';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/*" element={<App />} />
-        </Routes>
-        <ToastContainer
-          position="top-right"
-          autoClose={3000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnHover
-          limit={3}
-          theme="colored"
-          role="assertive"
-        />
-      </BrowserRouter>
-    </Provider>
+    <HelmetProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Helmet />
+          <Routes>
+            <Route path="/*" element={<App />} />
+          </Routes>
+          <ToastContainer
+            position="top-right"
+            autoClose={3000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnHover
+            limit={3}
+            theme="colored"
+            role="assertive"
+          />
+        </BrowserRouter>
+      </Provider>
+    </HelmetProvider>
   </React.StrictMode>
 );
 
